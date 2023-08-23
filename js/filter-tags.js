8c1196a2f76d1eda56cd89b0a7b5fca626c2147e
JamesHsu <aac219oo@gmail.com>
2023-08-23 15:39:19 +0800

    filter functoin adjust

--- js/filter-tags.js
+++ js/filter-tags.js
@@ -1,46 +1,48 @@
-filterSelection("all");
+filterSelection('filterDiv');
 function filterSelection(c) {
-  var x, y, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
+    var x, y, i;
+    x = document.getElementsByClassName('filterDiv');
+    // if (c == 'filterDiv') c = '';
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], 'show');
+        if (x[i].classList.contains(c)) {
+            w3AddClass(x[i], 'show');
+        }
+    }
 }
 
 function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+    var i, arr1, arr2;
+    arr1 = element.className.split(' ');
+    arr2 = name.split(' ');
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += ' ' + arr2[i];
+        }
     }
-  }
 }
 
 function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    var i, arr1, arr2;
+    arr1 = element.className.split(' ');
+    arr2 = name.split(' ');
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
     }
-  }
-  element.className = arr1.join(" ");
+    element.className = arr1.join(' ');
 }
 
 // Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
+var btnContainer = document.getElementById('myBtnContainer');
+var btns = btnContainer.getElementsByClassName('btn');
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+    btns[i].addEventListener('click', function () {
+        var current = document.getElementsByClassName('active');
+        current[0].className = current[0].className.replace(' active', '');
+        this.className += ' active';
+    });
 }
 
 // $(document).ready(function() {
@@ -51,7 +53,6 @@ for (var i = 0; i < btns.length; i++) {
 //   }
 // });
 
-
 // $(document).ready(function() {
 //   var listElement = $('.list');
 
@@ -60,7 +61,7 @@ for (var i = 0; i < btns.length; i++) {
 //   }
 // });
 
-   // 添加或移除.noshow类的函数
+// 添加或移除.noshow类的函数
 function toggleNoShowClass() {
     var listElements = document.querySelectorAll('.list');
     var dayElements = document.querySelectorAll('.day');
